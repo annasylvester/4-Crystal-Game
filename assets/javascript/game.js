e3253fbb1b889af9a771ebfbe0f77de34d64ec8a
annasylvester <annaleesylvester@gmail.com>
2018-12-13 02:02:33 -0500

    Updated js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,35 +6,93 @@
 // If guessed total > Random Num, then the player loses
 // Game restarts when player wins or loses
         
-//---------------
-let randomNum = "";
+
+// RESET RANDOM NUM
+
+let randomNum = [];
 let crystalVal = [];
-let crystal1 = "";
-let crystal2 = "";
-let crystal3 = "";
-let crystal4 = "";
 let guessedTotal = [];
+let addedTotal = [];
 let winCounter = 0;
 let lossCounter = 0;
-//----------------
 
-// Reset Random Number
+function reset() {
 
-function resetNum() {
+    //random number at top
     randomNum = Math.floor(Math.random() * 120) + 19;
     console.log(randomNum);
     document.getElementById("randomNum").innerHTML = randomNum;
 
-};
-resetNum();
-
-// Value of Crystals reset
-
-while(crystalVal.length < 4){
-    let r = Math.floor(Math.random() *12) +1;
-    if(crystalVal.indexOf(r) === -1) crystalVal.push(r);
+    //crystal values
+    while(crystalVal.length < 4){
+      let r = Math.floor(Math.random() *12) +1;
+      if(crystalVal.indexOf(r) === -1) {
+        crystalVal.push(r);
+      };
+    };
     console.log(crystalVal);
-}
+};
+
+reset();
+
+
+function doMath() {
+  let value1 = parseInt(guessedTotal.shift());
+  let value2 = parseInt(guessedTotal.shift());
+  let result = plus(value1, value2);
+  guessedTotal.push(result);
+  winLose();
+  console.log(guessedTotal);
+};
+
+function plus(a, b) {
+  return a + b;
+};
+
+function winLose() {
+  if (guessedTotal == randomNum) {
+    alert("You win!");
+    winCounter ++;
+  }
+
+  if (guessedTotal > randomNum) {
+    alert("You lost!");
+    lossCounter --;
+
+  }
+  console.log(winCounter, lossCounter);
+};
+
+// WHEN BUTTONS ARE CLICKED
+
+$("#crystal1").on("click", function () {
+    guessedTotal.push(crystalVal[0]);
+    console.log(guessedTotal);
+    if (guessedTotal.length > 1) {
+      doMath();
+    };
+  });
+  $("#crystal2").on("click", function () {
+    guessedTotal.push(crystalVal[1]);
+    console.log(guessedTotal);
+    if (guessedTotal.length > 1) {
+      doMath();
+    };
+  });
+  $("#crystal3").on("click", function () {
+    guessedTotal.push(crystalVal[2]);
+    console.log(guessedTotal);
+    if (guessedTotal.length > 1) {
+      doMath();
+    };
+  });
+  $("#crystal4").on("click", function () {
+    guessedTotal.push(crystalVal[3]);
+    console.log(guessedTotal);
+    if (guessedTotal.length > 1) {
+      doMath();
+    };
+  });
 
 
 
